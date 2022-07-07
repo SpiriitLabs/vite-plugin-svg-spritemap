15cd7c77eb749c08701e8b2028fa1dc1829a5890
Loïs Boubault <l.boubault@choosit.com>
2022-07-07 09:43:17 +0200

    add reload page on svg change

--- src/plugins/dev.ts
+++ src/plugins/dev.ts
@@ -5,6 +5,8 @@ import { generateSpritemap } from '../generateSpritemap'
 
 export function DevPlugin(options: Options) {
   let spritemap: string | false = false
+  let timeout: NodeJS.Timeout
+  let timestamp: string
 
   return <Plugin>{
     name: 'vite-plugin-svg-spritemap:dev',
@@ -19,7 +21,7 @@ export function DevPlugin(options: Options) {
         if (!spritemap) {
           spritemap = await generateSpritemap(options.icons)
         }
-        if (req.url === '/spritemap.svg' && spritemap) {
+        if (req.url?.startsWith('/__spritemap') && spritemap) {
           res.statusCode = 200
           res.setHeader('Content-Type', 'image/svg+xml')
           res.write(spritemap, 'utf-8')
@@ -29,8 +31,29 @@ export function DevPlugin(options: Options) {
         }
       })
     },
-    handleHotUpdate() {
+    transformIndexHtml: {
+      enforce: 'post',
+      async transform(html) {
+        if (!timestamp) {
+          return html
+        } else {
+          return html.replace(
+            /__spritemap-\d*|__spritemap/g,
+            `__spritemap-${timestamp}`
+          )
+        }
+      }
+    },
+    handleHotUpdate(ctx) {
       //generate new spritemap on icon changes
+      if (ctx.file.endsWith('.svg')) {
+        clearTimeout(timeout)
+        timeout = setTimeout(async () => {
+          spritemap = await generateSpritemap(options.icons)
+          timestamp = Date.now().toString()
+          ctx.server.ws.send({ type: 'full-reload' })
+        })
+      }
     }
   }
 }
