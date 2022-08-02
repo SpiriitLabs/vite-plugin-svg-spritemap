ac8f124ccba3723cc0af445d2dae49f8eff74ead
Loïs Boubault <7613286+Applelo@users.noreply.github.com>
2022-08-02 18:10:39 +0200

    fix styles tests

--- test/styles.test.ts
+++ test/styles.test.ts
@@ -1,26 +1,29 @@
 import { resolve } from 'path'
+import { promises as fs } from 'fs'
 import { build } from 'vite'
 import { it, describe, expect } from 'vitest'
 import VitePluginSvgSpritemap from '../src'
 
 const buildVite = async (style: string) => {
+  const filename = resolve(__dirname, `./project/styles/spritemap.${style}`)
   await build({
     root: resolve(__dirname, './project'),
     plugins: [
       VitePluginSvgSpritemap(resolve(__dirname, './project/svg/*.svg'), {
         styles: {
-          filename: resolve(__dirname, `./project/styles/spritemap.${style}`)
+          filename
         }
       })
     ]
   })
+  return fs.readFile(filename, 'utf8')
 }
 
 describe('Styles generation', () => {
-  for (const style of ['scss', 'less', 'stylus']) {
+  for (const style of ['scss', 'less', 'styl']) {
     it(style, async () => {
-      await buildVite('scss')
-      expect(true).toBe(true)
+      const result = await buildVite(style)
+      expect(result).toMatchSnapshot()
     })
   }
 })
