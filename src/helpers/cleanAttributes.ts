c94ef06595496e3668d9b76ecd8dfe6db68c70ce
Simeon Simeonoff <sim.simeonoff@gmail.com>
2023-04-12 11:37:29 +0300

    fix: Import assertion errors on typescript build Closes #2

--- src/helpers/cleanAttributes.ts
+++ src/helpers/cleanAttributes.ts
@@ -1,4 +1,4 @@
-import svgElementAttributes from 'svg-element-attributes'
+import svgElementAttributes from 'svg-element-attributes' assert { type: 'json' }
 
 export const cleanAttributes = (attributes: Attr[], tag: string): Attr[] => {
   const cleanAttributes = ['viewbox', 'width', 'height', 'id', 'xmlns']
