a5f035103a62b66760ef32321221962130e55b68
Loïs Boubault <7613286+Applelo@users.noreply.github.com>
2023-04-18 17:51:52 +0200

    move svg-element-attributes as a dev dependencies

--- tsup.config.ts
+++ tsup.config.ts
@@ -5,7 +5,6 @@ import fg from 'fast-glob'
 export default defineConfig({
   entry: ['src/index.ts'],
   dts: true,
-  noExternal: ['svg-element-attributes'],
   format: ['cjs', 'esm'],
   async onSuccess() {
     const files = await fg('src/styles/*.(scss|styl|less)')
