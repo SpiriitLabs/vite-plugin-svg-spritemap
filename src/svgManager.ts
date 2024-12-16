dd72ad02935beb46afed38d2d77de2d3d5b327c9
Loïs Boubault <7613286+Applelo@users.noreply.github.com>
2024-12-16 17:14:55 +0100

    replace fast-glob by tinnyglobby

--- src/svgManager.ts
+++ src/svgManager.ts
@@ -4,8 +4,8 @@ import type { Options, Pattern, SvgMapObject } from './types'
 import { promises as fs } from 'node:fs'
 import { basename, resolve } from 'node:path'
 import { DOMImplementation, DOMParser, XMLSerializer } from '@xmldom/xmldom'
-import fg from 'fast-glob'
 import hash_sum from 'hash-sum'
+import { glob } from 'tinyglobby'
 import { calculateY } from './helpers/calculateY'
 import { cleanAttributes } from './helpers/cleanAttributes'
 import { getOptimize, getOptions } from './helpers/svgo'
@@ -96,7 +96,7 @@ export class SVGManager {
   }
 
   async updateAll() {
-    const iconsPath = await fg(this._iconsPattern)
+    const iconsPath = await glob(this._iconsPattern)
 
     for (let index = 0; index < iconsPath.length; index++) {
       const iconPath = iconsPath[index]
