2dc03142c036d62a514bf7cf19767c6b803abbd7
Loïs <7613286+Applelo@users.noreply.github.com>
2025-10-14 18:19:04 +0200

    finalize hmr test

--- src/plugins/vue.ts
+++ src/plugins/vue.ts
@@ -25,18 +25,21 @@ export default function CommonPlugin(shared: Shared): Plugin {
         const { base: filename } = parse(path)
         const svg = svgManager?.svgs.get(path)
 
+        if (!svg)
+          return
+
         let source = ''
 
         if (query === 'view' && (options.output.view === false || options.output.use === false)) {
           config.logger.warn(`[vite-plugin-svg-spritemap] You need to enable the output.view and the output.use option to load ${id} as component with the ?view query.`)
         }
         else if (query === 'view') {
-          const width = svg?.width ? `width="${Math.ceil(svg.width)}"` : ''
-          const height = svg?.width ? `height="${Math.ceil(svg.height)}"` : ''
-          source = `<img src="/${options.route}#${options.prefix + svg?.id}-view" ${[width, height].filter(item => item.length > 0).join(' ')}/>`
+          const width = svg.width ? `width="${Math.ceil(svg.width)}"` : ''
+          const height = svg.height ? `height="${Math.ceil(svg.height)}"` : ''
+          source = `<img src="/${options.route}#${options.prefix + svg.id}-view" ${[width, height].filter(item => item.length > 0).join(' ')}/>`
         }
         else {
-          source = `<svg><slot/><use xlink:href="/${options.route}#${options.prefix + svg?.id}"></use></svg>`
+          source = `<svg><slot/><use xlink:href="/${options.route}#${options.prefix + svg.id}"></use></svg>`
         }
 
         const { compileTemplate } = await import('vue/compiler-sfc')
